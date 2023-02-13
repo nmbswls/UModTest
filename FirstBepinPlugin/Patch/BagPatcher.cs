2669120bf5d55c2cf7adf16bb84b8d2749124b1b
chenyexuan <chenyexuan@blackjack-inc.com>
2023-02-13 19:34:19 +0800

    支持隐藏物品标签

--- FirstBepinPlugin/Patch/BagPatcher.cs
+++ FirstBepinPlugin/Patch/BagPatcher.cs
@@ -19,6 +19,8 @@ namespace FirstBepinPlugin.Patch
     {
         public static void Postfix(UIInventory __instance)
         {
+            PluginMain.Main.LogError("BaseBag2Patcher_LoadPlayerItems");
+
             int numOfHide = 0;
             for(int i=0;i< __instance.InventoryLayout.GridDataList.Count;i++)
             {
@@ -30,8 +32,8 @@ namespace FirstBepinPlugin.Patch
                 if (itemJsonData.ItemFlag.Contains(-99))
                 {
                     numOfHide++;
+                    __instance.InventoryLayout.GridDataList[i] = null;
                 }
-                __instance.InventoryLayout.GridDataList[i] = null;
             }
             for (int i = __instance.InventoryLayout.GridDataList.Count - 1; i >=0; i--)
             {
@@ -43,6 +45,30 @@ namespace FirstBepinPlugin.Patch
         }
     }
 
+    /// <summary>
+    /// 背包扩展 隐藏道具
+    /// </summary>
+    [HarmonyPatch(typeof(BaseBag2), "FiddlerItem")]
+    public class BaseBag2Patcher_FiddlerItem
+    {
+        public static void Postfix(BaseBag2 __instance, BaseItem baseItem, ref bool __result)
+        {
+
+            if (__result == false)
+            {
+                return;
+            }
+
+            var itemJsonData = _ItemJsonData.DataDict[baseItem.Id];
+            if (itemJsonData != null && itemJsonData.ItemFlag.Contains(-99))
+            {
+                PluginMain.Main.LogError("TabBag FiddlerItem false.");
+                __result = false;
+            }
+        }
+    }
+
+
 
     /// <summary>
     /// 背包扩展 隐藏道具
@@ -52,6 +78,7 @@ namespace FirstBepinPlugin.Patch
     {
         public static void Postfix(TabBag __instance, BaseItem baseItem, ref bool __result)
         {
+
             if(__result == false)
             {
                 return;
@@ -65,4 +92,17 @@ namespace FirstBepinPlugin.Patch
             }
         }
     }
+    /// <summary>
+    /// 背包扩展 隐藏道具
+    /// </summary>
+    [HarmonyPatch(typeof(TabBag), "OpenBag")]
+    public class TabBagPatcher_OpenBag
+    {
+        public static void Postfix(TabBag __instance, BagType bagType)
+        {
+            __instance.UpdateItem();
+        }
+    }
+    
 }
+
